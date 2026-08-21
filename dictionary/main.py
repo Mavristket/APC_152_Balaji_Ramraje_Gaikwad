3086267b444dca754dfe026aec5d9877ee70f0db
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-08-21 14:44:10 +0530

    Create main.py

---
+++ dictionary/main.py
@@ -0,0 +1,12 @@
+
+student = {
+    "name": "Balaji",
+    "age": 21,
+    "branch": "CSE"
+}
+
+print(student)
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Branch:", student["branch"])
